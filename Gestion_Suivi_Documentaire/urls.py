0052ba020d0d80968c370fc3dedf57b35c6c7258
Passing01 <ouerahim456@gmail.com>
2026-07-01 19:01:42 +0000

    modification

--- Gestion_Suivi_Documentaire/urls.py
+++ Gestion_Suivi_Documentaire/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from core.views import index
 from core.views_api import AppSettingsViewSet
 from rest_framework.routers import DefaultRouter
@@ -15,3 +17,6 @@ urlpatterns = [
     path('api/analytics/', include('analytics.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
